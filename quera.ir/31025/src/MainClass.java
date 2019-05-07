63d73e8ea7302c156d17fa47a5221549e8d2eb8d
ali <aligator2236@gmail.com>
2019-05-07 13:07:55 +0430

    quera_31025 - refactored the code

--- quera.ir/31025/src/MainClass.java
+++ quera.ir/31025/src/MainClass.java
@@ -2,23 +2,15 @@ import java.util.Scanner;
 import java.util.Timer;
 
 public class MainClass {
-    static int n,k;
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String[] ss = s.nextLine().split(" ");
-        long f = System.nanoTime();
-        n = Integer.valueOf(ss[0]);
-        k = Integer.valueOf(ss[1]);
 
-        System.out.println(divide(n,k));
-        System.err.println((System.nanoTime() - f)/Math.pow(10,6) + " ms");
+        int n = s.nextInt(), k = s.nextInt();
+
+        System.out.println((int) Math.floor(n / Math.pow(2, k)));
 
 
-    }
-
-    public static int divide(int n,int k){
-        double m = (n / Math.pow(2,k));
-        return (int)Math.floor(m);
     }
 
 }
