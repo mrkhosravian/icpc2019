926a726acd0141aef4ed0a67a0011a23fb86ce48
ali <aligator2236@gmail.com>
2019-05-25 09:20:48 +0430

    added one algorithm and one problem

---
+++ exercise/src/mathematics/algorithms/MathematicalExpressions.java
@@ -0,0 +1,26 @@
+package mathematics.algorithms;
+
+import annotations.Algorithm;
+import annotations.Persian;
+
+public class MathematicalExpressions {
+
+    @Persian("پیشوندی")
+    static void prefix(){
+
+    }
+
+    @Persian("میان بندی")
+    static void infix(){
+
+    }
+
+    @Persian("پسوندی")
+    static void postfix(){
+
+    }
+
+    static void InfixToPostfix(){
+
+    }
+}
---
+++ exercise/src/mathematics/problems/Equation.java
@@ -0,0 +1,14 @@
+package mathematics.problems;
+
+import annotations.Algorithm;
+import annotations.TimeLimit;
+import annotations.Title;
+import annotations.URL;
+import mathematics.algorithms.MathematicalExpressions;
+
+@Algorithm(MathematicalExpressions.class)
+@Title("727 - Equation")
+@URL("https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=9&page=show_problem&problem=668")
+@TimeLimit(3.000)
+public class Equation {
+}
